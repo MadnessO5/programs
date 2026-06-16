e0d5273bfb5f76c27af5f1d78686913016fefcb6
Madness_O5 <ditabaskov@gmail.com>
2026-06-16 14:07:23 +0700

    Add files via upload

---
+++ PascalPrograms/function/fun2/fun_2.pas
@@ -0,0 +1,29 @@
+program Fi;
+var
+	n: integer;
+	a: longint;
+function Fibonacci(n: integer): longint;
+var
+    i: integer;
+    p, q, r: longint;
+begin
+    if n <= 0 then
+        Fibonacci := 0
+    else
+    begin
+        q := 0;
+        r := 1;
+        for i := 2 to n do
+        begin
+            p := q;
+            q := r;
+            r := p + q
+        end;
+        Fibonacci := r
+    end;
+end;
+begin
+	readln(n);
+	a := Fibonacci(n);
+	writeln(a);
+end.
